ce2b85959c5ae4045bc8a6c4271fcd0e000ac7bc
a2tikhonov <git!St@d1um@rc@d1um>
2022-05-28 01:45:39 +0300

    Сделал домашку

--- src/main/java/ru/atikhonov/course2/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/ru/atikhonov/course2/calculator/service/CalculatorServiceImpl.java
@@ -11,7 +11,7 @@ public class CalculatorServiceImpl implements CalculatorService{
         return "Добро пожаловать в калькулятор";
     }
 
-    private String print(float num1, char operator, float num2, float result) {
+    private String formatExpression(float num1, char operator, float num2, float result) {
         DecimalFormat format = new DecimalFormat();
         format.setDecimalSeparatorAlwaysShown(false);
         return String.format("%s %c %s = %s", format.format(num1), operator,
@@ -21,19 +21,19 @@ public class CalculatorServiceImpl implements CalculatorService{
     @Override
     public String add(float num1, float num2) {
         float result = num1 + num2;
-        return print(num1, '+', num2, result);
+        return formatExpression(num1, '+', num2, result);
     }
 
     @Override
     public String substract(float num1, float num2) {
         float result = num1 - num2;
-        return print(num1, '-', num2, result);
+        return formatExpression(num1, '-', num2, result);
     }
 
     @Override
     public String multiply(float num1, float num2) {
         float result = num1 * num2;
-        return print(num1, '*', num2, result);
+        return formatExpression(num1, '*', num2, result);
     }
 
     @Override
@@ -42,7 +42,7 @@ public class CalculatorServiceImpl implements CalculatorService{
             return "Деление на ноль невозможно!";
         }
         float result = num1 / num2;
-        return print(num1, '/', num2, result);
+        return formatExpression(num1, '/', num2, result);
     }
 
 }
