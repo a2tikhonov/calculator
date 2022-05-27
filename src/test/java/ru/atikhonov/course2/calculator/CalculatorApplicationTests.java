9495cbd5023676ae8a140585624adc1e7fc0c313
a2tikhonov <git!St@d1um@rc@d1um>
2022-05-27 19:34:54 +0300

    Initial commit

---
+++ src/test/java/ru/atikhonov/course2/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.atikhonov.course2.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
